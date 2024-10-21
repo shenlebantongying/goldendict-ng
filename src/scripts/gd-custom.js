5b64d2f9770770b1aaf6d8035f46ffe4a82a6176
shenleban tongying <shenlebantongying@gmail.com>
2024-10-21 08:37:39 -0400

    delay `gd-custom.js` to after `DOMContentLoaded`

--- src/scripts/gd-custom.js
+++ src/scripts/gd-custom.js
@@ -1,68 +1,76 @@
 //document ready
-(function ($) {
-  $(function () {
-    $(document).on("click", "a", function (event) {
-      var link = $(this).attr("href");
-      if ("string" != typeof link) {
-        return;
-      }
+function gd_customize() {
+  $(document).on("click", "a", function (event) {
+    var link = $(this).attr("href");
+    if ("string" != typeof link) {
+      return;
+    }
 
-      if (link.indexOf("javascript:") >= 0) {
-        return;
-      }
+    if (link.indexOf("javascript:") >= 0) {
+      return;
+    }
 
-      //return if the link is like gdlookup:// or other valid url.
-      if (link.indexOf(":") >= 0) {
-        emitClickedEvent(link);
-        return false;
-      }
-      emitClickedEvent("");
+    //return if the link is like gdlookup:// or other valid url.
+    if (link.indexOf(":") >= 0) {
+      emitClickedEvent(link);
+      return false;
+    }
+    emitClickedEvent("");
 
-      var newLink;
-      var href = window.location.href;
-      var index = -1;
-      if (link.startsWith("#")) {
-        //the href may contain # fragment already.remove them before append the new #fragment
-        index = href.indexOf("#");
-        if (index > -1) {
-          newLink = href.substring(0, index) + link;
-        } else {
-          newLink = href + link;
-        }
-      }
-      //if hashtag # is not in the start position,it must greater than 0
-      else if (link.indexOf("#") > 0) {
-        index = link.indexOf("#");
-        newLink =
-          "gdlookup://localhost/" +
-          link.substring(0, index) +
-          "?gdanchor=" +
-          link.substring(index + 1);
+    var newLink;
+    var href = window.location.href;
+    var index = -1;
+    if (link.startsWith("#")) {
+      //the href may contain # fragment already.remove them before append the new #fragment
+      index = href.indexOf("#");
+      if (index > -1) {
+        newLink = href.substring(0, index) + link;
       } else {
-        index = href.indexOf("?");
+        newLink = href + link;
+      }
+    }
+    //if hashtag # is not in the start position,it must greater than 0
+    else if (link.indexOf("#") > 0) {
+      index = link.indexOf("#");
+      newLink =
+        "gdlookup://localhost/" +
+        link.substring(0, index) +
+        "?gdanchor=" +
+        link.substring(index + 1);
+    } else {
+      index = href.indexOf("?");
 
-        if (link.indexOf("?gdanchor") > -1) {
-          newLink = "gdlookup://localhost/" + link;
+      if (link.indexOf("?gdanchor") > -1) {
+        newLink = "gdlookup://localhost/" + link;
+      } else {
+        if (index > -1) {
+          newLink = href.substring(0, index) + "?word=" + link;
         } else {
-          if (index > -1) {
-            newLink = href.substring(0, index) + "?word=" + link;
-          } else {
-            newLink = href + "?word=" + link;
-          }
+          newLink = href + "?word=" + link;
         }
       }
-      $(this).attr("href", newLink);
-    });
-
-    //monitor iframe height.
-    $("iframe").iFrameResize({
-      checkOrigin: false,
-      maxHeight: 800,
-      scrolling: true,
-      warningTimeout: 0,
-      minHeight: 250,
-      log: true,
-      autoResize: false,
-    });
+    }
+    $(this).attr("href", newLink);
   });
-})(jQuery);
+
+  //monitor iframe height.
+  $("iframe").iFrameResize({
+    checkOrigin: false,
+    maxHeight: 800,
+    scrolling: true,
+    warningTimeout: 0,
+    minHeight: 250,
+    log: true,
+    autoResize: false,
+  });
+}
+
+function gd_page_ready_fn(f){
+if (document.readyState !== "loading") {
+  f();
+} else {
+  document.addEventListener("DOMContentLoaded", f);
+};
+};
+
+gd_page_ready_fn(gd_customize);
